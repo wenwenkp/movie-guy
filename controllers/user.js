2c32c5dec19d6bd8c048c3eba36afa53a3feda12
wenjie li <wenjie@wenjies-MacBook-Pro.local>
2019-10-05 01:31:41 -0700

    add signup method and create token

--- controllers/user.js
+++ controllers/user.js
@@ -1,14 +1,31 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.SECRET;
 
 module.exports = {
     signup,
     login,
 };
 
-function signup(req, res){
-
+async function signup(req, res){
+    const user = new User(req.body);
+    try{
+        await user.save();
+        const token = createJWT(user);
+        res.json({token});
+    }catch(err){
+        res.status(400).json(err);
+    }
 }
 
 function login(req, res){
 
+}
+
+function createJWT(user){
+    return jwt.sign(
+        {user},
+        SECRET,
+        {expiresIn: '12h'}
+    );
 }
