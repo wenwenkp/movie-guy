f02ee2a4f0eb8b00621f35d383279d92b9c85b8d
wenjie li <wenjie@wenjies-MacBook-Pro.local>
2019-10-07 10:30:18 -0700

    fix bug in run build

--- src/services/movie_api.js
+++ src/services/movie_api.js
@@ -1,11 +1,20 @@
-// const API_KEY = process.env.API_KEY;
+// const dotenv = require('dotenv')
+// const buf = Buffer.from('BASIC=basic')
+// const config = dotenv.parse(buf) // will return an object
+// const api = dotenv
+// console.log(typeof config, config) // object { BASIC : 'basic' }
+
+// const API_KEY = process.env.REACT_APP_API_KEY;
 const API_KEY='90ce4aaf41910d53cce3f11e3053992f';
+console.log(API_KEY);
+
 
 const apiBaseUrl = 'http://api.themoviedb.org/3';
 const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${API_KEY}`;
 const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
 
 async function getNowPlaying() {
+    // console.log(API_KEY);
     let result = await fetch(nowPlayingUrl, { mode: 'cors' }).then(response => response.json());
     let nowPlayingMovies = result.results;
     let nowPlaying = nowPlayingMovies.map((movie)=>{
@@ -17,14 +26,13 @@ async function getNowPlaying() {
 
 async function getMovie(id) {
     let movieId = id;
-    console.log(id);
     const movieUrl = `${apiBaseUrl}/movie/${movieId}?api_key=${API_KEY}`;
     let movie = await fetch(movieUrl, { mode: 'cors' }).then(response => response.json());
     movie.poster_path = imageBaseUrl + movie.poster_path;
     return movie;
 };
 
-module.exports = {
+export default {
     getNowPlaying,
     getMovie,
 };
