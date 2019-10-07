1d236ec97a2a38f374f977a25fa5abeaeaf660b9
wenjie li <wenjie@wenjies-MacBook-Pro.local>
2019-10-07 15:36:33 -0700

    edit movie detail pagte

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -16,15 +16,44 @@ class Movie extends React.Component {
     }
 
     render() {
+        let languages = this.state.movie.spoken_languages;
+        let genres = this.state.movie.genres;
+        let companies = this.state.movie.production_companies;
         return (
             <div>
                 {this.state.movie ?
                     <div>
                         <h1>{this.state.movie.title}</h1>
                         <img src={this.state.movie.poster_path} alt={this.state.movie.title}></img>
-                        <p>{this.state.movie.status}</p>
-                        <p>{this.state.movie.release_date}</p>
-                        <p>{this.state.movie.overview}</p>
+                        <p>tagline: {this.state.movie.tagline}</p>
+                        <p>status: {this.state.movie.status}</p>
+                        <p>release date: {this.state.movie.release_date}</p>
+                        <p>budget: {this.state.movie.budget}</p>
+                        {languages ? 
+                        <ul>Languages: 
+                            {languages.map((language, idx)=>{
+                                return <li key={idx}>{language.name}</li>
+                            })}
+                        </ul>
+                        : ''
+                        }
+                        {genres ? 
+                        <ul>genres: 
+                            {genres.map((genre, idx)=>{
+                                return <li key={idx}>{genre.name}</li>
+                            })}
+                        </ul>
+                        : ''
+                        }
+                        {companies ? 
+                        <ul>Companies: 
+                            {companies.map((company, idx)=>{
+                                return <li key={idx}>{company.name}</li>
+                            })}
+                        </ul>
+                        : ''
+                        }
+                        <p>overview: {this.state.movie.overview}</p>
                     </div>
                     :
                     <h1>Loading...</h1>
