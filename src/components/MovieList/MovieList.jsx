895dcbe404960679d1ef6846e4d5f8cb819ba51b
wenjie li <wenjie@wenjies-MacBook-Pro.local>
2019-10-10 15:42:05 -0700

    set up movie list css

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,25 +3,35 @@ import { Link } from 'react-router-dom';
 
 const MovieList = (props) => {
     return (
-        <div className='container'>
+        <div>
             {props.movies ?
-            <div className='row'>
-                <div className='col-sm-12'>
-                    <h1>{props.title}</h1>
-                </div>
-                <div className='col-sm-12'>
+            <div className='list'>
+                <div className="list__text--box">
+                    <h1 className="list-primary">{props.title}</h1>
+                    {/* <div className='images-box'> */}
                     {props.movies.map((movie) => {
-                        if (movie.poster_path !== null) {
-                            return (
-                                <Link to={`/movie/${movie.id}`} key={movie.id}>
-                                    <img src={movie.poster_path} alt={movie.title}></img>
-                                </Link>
-                            )
-                        }else {
-                            return null;
-                        }
+                    if (movie.poster_path !== null) {
+                        let time = Math.random()*5;
+                        console.log(time);
+                        let imgStyle = {
+                            animationDuration: time + 's'
+                        };
+                    return (
+                    <Link to={`/movie/${movie.id}`} key={movie.id}>
+                        <img src={movie.poster_path} alt={movie.title} 
+                        className="img img--black img--animated"
+                        //  style={{`animation: ${time} + em`}}
+                        style={imgStyle}
+                          >
+
+                        </img>
+                    </Link>
+                    )
+                    }else {
+                    return null;
+                    }
                     })}
-                </div>
+                </div> 
             </div>
             : <div>'Loading...'</div>}
         </div>
