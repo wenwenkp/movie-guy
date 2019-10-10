d4b7ac8de48146bf4d92111a06f9d466f63ec1d3
wenjie li <wenjie@wenjies-MacBook-Pro.local>
2019-10-10 08:38:00 -0700

    add home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,11 +4,11 @@ const Home = () => {
     return (
         <div className="header">
             <div className="logo-box">
-                <img src="img/logo-white.png" alt="Logo" className="logo" />
+                <img src="#" alt="Logo" className="logo" />
             </div>
             <div className="text-box">
                 <h1 className="heading-primary">
-                    <span className="heading-primary-main">OUTDOORS</span>
+                    <span className="heading-primary-main">MOVIEGUY</span>
                     <span className="heading-primary-sub">is where life happens</span>
                 </h1>
             </div>
