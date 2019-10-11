3e40bec61431853e5ca818c97640d89b1fedc2d3
wenjie li <wenjie@wenjies-MacBook-Pro.local>
2019-10-11 13:15:49 -0700

    improve shadow for navbar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,11 +10,12 @@ const Home = (props) => {
                     <span className="heading-primary--main">MOVIEGUY</span>
                     <span className="heading-primary--sub">where happiness from</span>
                 </h1>
-                <Link to='/movies/now_playing'><img src={logo} alt="logo" className="logo logo--black logo--animated"></img></Link>
                 <form className="logo--animated" >
                     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" value={props.keyword} onChange={(e) => props.handleChange(e)} name="keyword" />
                      <Link to={`/search/${props.keyword}`}><button onClick={(e) => { props.handleSearch(e) }} className="btn btn-outline-success my-2 my-sm-0" type="submit" hidden>Search</button></Link>
                 </form>
+                <Link to='/movies/now_playing'><img src={logo} alt="logo" className="logo logo--black logo--animated"></img></Link>
+
             </div>
         </header>
     )
