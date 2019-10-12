0aa3d055e3ed8b56786276aacdd4cb547f6da06c
wenjie li <wenjie@wenjies-MacBook-Pro.local>
2019-10-12 12:31:31 -0700

    polish home css file

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,15 +3,15 @@ import {Link} from 'react-router-dom';
 
 const Home = (props) => {
     return (
-        <header className="header">
-            <div className="header__text--box">
+        <header className="home">
+            <div className="home__text--box">
                 <h1 className="heading-primary">
                     <span className="heading-primary--main">MOVIEGUY</span>
                     <span className="heading-primary--sub">Discover your movies</span>
                 </h1>
                 <form className="search search--animated" >
                     <input className="search__box" type="search" placeholder="&nbsp;&nbsp;from here..." aria-label="Search" value={props.keyword} onChange={(e) => props.handleChange(e)} name="keyword" />
-                     <Link to={`/search/${props.keyword}`}><button onClick={(e) => { props.handleSearch(e) }} className="btn btn-outline-success my-2 my-sm-0" type="submit" hidden>Search</button></Link>
+                     <Link to={`/search/${props.keyword}`}><button onClick={(e) => { props.handleSearch(e) }} type="submit" hidden></button></Link>
                 </form>
             </div>
         </header>
