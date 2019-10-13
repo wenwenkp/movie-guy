b07f5b9cb76dc810e145c4efdc346ed5b7f99f5b
wenjie li <wenjie@wenjies-MacBook-Pro.local>
2019-10-12 17:47:08 -0700

    add heart css

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -24,57 +24,82 @@ class Movie extends React.Component {
             for (let i = 0; i < this.props.myMovies.length; i++) {
                 if (this.props.myMovies[i].movieId === this.state.movie.id) {
                     option = true;
+                }
             }
         }
-    }
 
         return (
             <div>
                 {this.state.movie ?
-                    <div>
-                        <h1>{this.state.movie.title}</h1>
-                        <img src={this.state.movie.poster_path} alt={this.state.movie.title}></img>
-                        <p>tagline: {this.state.movie.tagline}</p>
-                        <p>status: {this.state.movie.status}</p>
-                        <p>release date: {this.state.movie.release_date}</p>
-                        <p>budget: {this.state.movie.budget}</p>
-                        {languages ?
-                            <ul>Languages:
-                            {languages.map((language, idx) => {
-                                return <li key={idx}>{language.name}</li>
-                            })}
-                            </ul>
-                            : ''
-                        }
-                        {genres ?
-                            <ul>genres:
-                            {genres.map((genre, idx) => {
-                                return <li key={idx}>{genre.name}</li>
-                            })}
-                            </ul>
-                            : ''
-                        }
-                        {companies ?
-                            <ul>Companies:
-                            {companies.map((company, idx) => {
-                                return <li key={idx}>{company.name}</li>
-                            })}
-                            </ul>
-                            : ''
-                        }
-                        <p>overview: {this.state.movie.overview}</p>
-                        {this.props.user ? 
-                            <div>
-                                {option ? 
-                                <button onClick={() => { this.props.removeFavMovie(this.state.movie.id) }}>Unsave</button>
-                                :
-                                <button onClick={() => { this.props.addFavMovie(this.state.movie) }}>Add to Favourite</button>}
+                    <>
+                        <div className="row detail--title">
+                            <h1>{this.state.movie.title}</h1>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <img src={this.state.movie.poster_path} alt={this.state.movie.title}></img>
+                                {this.props.user ?
+                                    <div className="heart--box">
+                                        <button>
+                                            <div className="heart--img"></div>
+                                        </button>
+                                        {option ?
+                                            <button onClick={() => { this.props.removeFavMovie(this.state.movie.id) }}>Unsave</button>
+                                            :
+                                            <button onClick={() => { this.props.addFavMovie(this.state.movie) }}>Add to Favourite</button>}
+                                    </div>
+                                    : null
+                                }
                             </div>
-                            : 
-                            null}
-                    </div>
-                    :
-                    <h1>Loading...</h1>
+
+                            <div className="col detail--content">
+                                <div>
+                                    <h2>tagline: </h2>
+                                    <span>{this.state.movie.tagline}</span>
+                                </div>
+                                <div>
+                                    <h2>status: </h2>
+                                    <span>{this.state.movie.status}</span>
+                                </div>
+                                <div>
+                                    <h2>release date: </h2>
+                                    <span>{this.state.movie.release_date}</span>
+                                </div>
+                                <div>
+                                    <h2>budget: </h2>
+                                    <span>{this.state.movie.budget}</span>
+                                </div>
+
+                                {languages ?
+                                    <ul><h2>Languages:</h2>
+                                        {languages.map((language, idx) => {
+                                            return <li key={idx}>{language.name}</li>
+                                        })}
+                                    </ul>
+                                    : ''
+                                }
+                                {genres ?
+                                    <ul><h2>genres:</h2>
+                                        {genres.map((genre, idx) => {
+                                            return <li key={idx}>{genre.name}</li>
+                                        })}
+                                    </ul>
+                                    : ''
+                                }
+                                {companies ?
+                                    <ul><h2>Companies:</h2>
+                                        {companies.map((company, idx) => {
+                                            return <li key={idx}>{company.name}</li>
+                                        })}
+                                    </ul>
+                                    : ''
+                                }
+                                <h2>overview: </h2>
+                                <p>{this.state.movie.overview}</p>
+                            </div>
+                        </div>
+                    </>
+                    : ''
                 }
             </div>
         )
